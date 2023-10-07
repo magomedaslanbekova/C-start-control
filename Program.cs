6ad5256ca44ab36c00b29116edf6f4a5d76b0e16
«Magomed» <magomed@example.com>
2023-10-07 13:47:20 +0300

    создал массив сделать так чтоб были рандомные слова и поставил условие if

--- Program.cs
+++ Program.cs
@@ -1,7 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+               
 
 
 
 
+int words = { "Hello", "World", "This", "Is", "An", "Example" };
+  
+    int random_word = rand() % 6;
+
+int rand()
+{
+    throw new NotImplementedException();
+}
+return 0;
+
+ if ( random_word <= 3);
+{
+    System.Console.WriteLine();
+}
 
